fc3b63060477dbbd30e42d466795ccb68f81ea52
Vikas Naithani <132071075+VikasNaithani@users.noreply.github.com>
2025-07-24 19:18:34 +0530

    Delete test/examples/ignore.spec.js

--- test/examples/ignore.spec.js
+++
@@ -1,9 +0,0 @@
-const assert = require("assert");
-const { qase } = require("mocha-qase-reporter/mocha");
-
-describe("Example: ignore.spec.js", function () {
-  it("This test is executed using Mocha; however, it is NOT reported to Qase", function () {
-    this.ignore();
-    assert.strictEqual(1, 1);
-  });
-});
