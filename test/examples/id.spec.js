dc04e9c11a544b51382d1aa7e7bf8ff9ebb47854
Vikas Naithani <132071075+VikasNaithani@users.noreply.github.com>
2025-07-24 19:18:24 +0530

    Delete test/examples/id.spec.js

--- test/examples/id.spec.js
+++
@@ -1,8 +0,0 @@
-const assert = require("assert");
-const { qase } = require("mocha-qase-reporter/mocha");
-
-describe("Example: id.spec.js", function () {
-  it(qase(1, "Test with Qase Id"), function () {
-    assert.strictEqual(1, 1);
-  });
-});
